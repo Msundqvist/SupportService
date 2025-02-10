b8a088ca334457e9e4855d8236d1bac136aba3c5
MatildaSundqvist <matildasundqvist@live.se>
2025-02-10 11:28:32 +0100

    finalized User class

--- user.js
+++ user.js
@@ -1,9 +1,10 @@
+
 export class User {
     constructor(email, caption, description) {
         this.email = email
         this.caption = caption
         this.description = description
-        this.issueDate = Date.now;
-        this.issueNumber = 1;
+        this.issueDate = new Date().toLocaleString('sv-SE');
+        this.issueNumber = crypto.randomUUID().replaceAll('-', '')
     }
 }
